199a4d7b0ee96a3d93e0079d55a161566614748a
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2022-02-26 14:34:40 +1100

    save 'first tags' preference

--- ReorderTags.omnifocusjs/Resources/preferences.js
+++ ReorderTags.omnifocusjs/Resources/preferences.js
@@ -9,22 +9,24 @@
     const lastTags = lib.getTags('last')
 
     // first form - multi-select
-    const firstForm = new Form()
-    firstForm.addField(new Form.Field.MultipleOptions('firstTags', 'First Tags', flattenedTags, flattenedTags.map(t => t.name), firstTags))
-    await firstForm.show('Select First Tags', 'Continue')
+    const firstSelectForm = new Form()
+    const firstFlattenedTags = [...firstTags, ...flattenedTags.filter(tag => !firstTags.includes(tag))]
+    firstSelectForm.addField(new Form.Field.MultipleOptions('firstTags', 'First Tags', firstFlattenedTags, firstFlattenedTags.map(t => t.name), firstTags))
+    await firstSelectForm.show('SELECT FIRST TAGS.\nThese are tags that should be put first when the \'Reorder Tags\' action is run. \nIf more than one tag is selected, you will be able to select the order in the next screen.', 'Continue')
 
-    const secondForm = lib.reorderForm(firstForm.values.firstTags)
-
-    await secondForm.show('Order First Tags', 'OK')
-
-    const orderedTags = secondForm.fields.map(field => Tag.byIdentifier(field.key))
+    if (firstSelectForm.values.firstTags.length > 0) {
+      const firstOrderForm = lib.reorderForm(firstSelectForm.values.firstTags)
+      await firstOrderForm.show('Order First Tags', 'OK')
+      const orderedTags = firstOrderForm.fields.map(field => Tag.byIdentifier(field.key))
+      syncedPrefs.write('firstTagIDs', orderedTags.map(tag => tag.id.primaryKey))
+    } else syncedPrefs.write('firstTagIDs', [])
 
     
 
 
+  
 
 
-    // TODO: save preferences
 
   })
 
