7a9caf7801b68a396791ebdf3367c1a493246626
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2022-02-26 14:40:12 +1100

    don't show re-order form when only one tag selected

--- ReorderTags.omnifocusjs/Resources/preferences.js
+++ ReorderTags.omnifocusjs/Resources/preferences.js
@@ -8,18 +8,18 @@
     const firstTags = lib.getTags('first')
     const lastTags = lib.getTags('last')
 
-    // first form - multi-select
+    // configure first tags
     const firstSelectForm = new Form()
     const firstFlattenedTags = [...firstTags, ...flattenedTags.filter(tag => !firstTags.includes(tag))]
     firstSelectForm.addField(new Form.Field.MultipleOptions('firstTags', 'First Tags', firstFlattenedTags, firstFlattenedTags.map(t => t.name), firstTags))
     await firstSelectForm.show('SELECT FIRST TAGS.\nThese are tags that should be put first when the \'Reorder Tags\' action is run. \nIf more than one tag is selected, you will be able to select the order in the next screen.', 'Continue')
 
-    if (firstSelectForm.values.firstTags.length > 0) {
+    if (firstSelectForm.values.firstTags.length > 1) {
       const firstOrderForm = lib.reorderForm(firstSelectForm.values.firstTags)
       await firstOrderForm.show('Order First Tags', 'OK')
       const orderedTags = firstOrderForm.fields.map(field => Tag.byIdentifier(field.key))
       syncedPrefs.write('firstTagIDs', orderedTags.map(tag => tag.id.primaryKey))
-    } else syncedPrefs.write('firstTagIDs', [])
+    } else syncedPrefs.write('firstTagIDs', firstSelectForm.values.firstTags.map(tag => tag.id.primaryKey))
 
     
 
